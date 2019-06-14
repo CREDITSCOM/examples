f72d3123d491fede64842122b0f4d3c4c4acb40c
pvl1175 <vitaly.popov.75@gmail.com>
2019-06-14 18:55:23 +0300

    update

--- API/CreditsCSharpDemo/Program.cs
+++ API/CreditsCSharpDemo/Program.cs
@@ -1,10 +1,12 @@
 ﻿using Chaos.NaCl;
 using NodeApi;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using Thrift.Protocol;
 using Thrift.Transport;
+using SauceControl.Blake2Fast;
 
 namespace CreditsCSAPIDemo
 {
@@ -46,6 +48,11 @@ namespace CreditsCSAPIDemo
             return client.TransactionFlow(CreateTransaction());
         }
 
+        public TransactionFlowResult ExecuteTransactionWithSmartContract(string smCode)
+        {
+            return client.TransactionFlow(CreateTransactionWithSmartContract(smCode));
+        }
+
         private Transaction CreateTransaction()
         {
             var transaction = new Transaction();
@@ -55,6 +62,57 @@ namespace CreditsCSAPIDemo
             transaction.Amount = new Amount(1, 0);
             transaction.Fee = new AmountCommission(Fee(0.9));
             transaction.Currency = 1;
+
+            var bytes = new byte[86];
+            Array.Copy(BitConverter.GetBytes(transaction.Id), 0, bytes, 0, 6);
+            Array.Copy(transaction.Source, 0, bytes, 6, 32);
+            Array.Copy(transaction.Target, 0, bytes, 38, 32);
+            Array.Copy(BitConverter.GetBytes(transaction.Amount.Integral), 0, bytes, 70, 4);
+            Array.Copy(BitConverter.GetBytes(transaction.Amount.Fraction), 0, bytes, 74, 8);
+            Array.Copy(BitConverter.GetBytes(transaction.Fee.Commission), 0, bytes, 82, 2);
+            bytes[84] = 1;
+            bytes[85] = 0;
+
+            var signature = Ed25519.Sign(bytes, sourceKeys.PrivateKeyBytes);
+            var verifyResult = Ed25519.Verify(signature, bytes, sourceKeys.PublicKeyBytes);
+            if (!verifyResult) throw new Exception("Signature could not be verified");
+
+            transaction.Signature = signature;
+            return transaction;
+        }
+
+        private Transaction CreateTransactionWithSmartContract(string smCode)
+        {
+            if (smCode == "")
+                smCode =
+                "import com.credits.scapi.annotations.*; import com.credits.scapi.v0.*; public class MySmartContract extends SmartContract { public MySmartContract() {} public String hello() { return \"Hello\"; } }";
+
+            var transaction = new Transaction();
+            transaction.Id = client.WalletTransactionsCountGet(sourceKeys.PublicKeyBytes).LastTransactionInnerId + 1;
+            transaction.Source = sourceKeys.PublicKeyBytes;
+            transaction.Target = targetKeys.PublicKeyBytes;
+            transaction.Amount = new Amount(0, 0);
+            transaction.Fee = new AmountCommission(Fee(2.0));
+            transaction.Currency = 1;
+
+            List<byte> target = new List<byte>(transaction.Source);
+            target.AddRange(BitConverter.GetBytes(transaction.Id));
+            var byteCode = client.SmartContractCompile(smCode);
+
+            if (byteCode.Status.Code == 0)
+            {
+                for (int i = 0; i < byteCode.ByteCodeObjects.Count; i++)
+                {
+                    target.AddRange(byteCode.ByteCodeObjects[i].ByteCode);
+                }
+            }
+            else
+            {
+                Console.WriteLine(byteCode.Status.Message);
+                return null;
+            }
+
+            transaction.Target = SauceControl.Blake2Fast.Blake2s.ComputeHash(target.ToArray());
 
             var bytes = new byte[86];
             Array.Copy(BitConverter.GetBytes(transaction.Id), 0, bytes, 0, 6);
@@ -134,7 +192,8 @@ namespace CreditsCSAPIDemo
 
                         var clientEx = new ClientEx(client, SourceKeys, TargetKeys);
                         Console.WriteLine("Result of the transaction execution:");
-                        Console.WriteLine(clientEx.ExecuteTransaction());
+                        //Console.WriteLine(clientEx.ExecuteTransaction());
+                        Console.WriteLine(clientEx.ExecuteTransactionWithSmartContract(""));
                     }
                 }
             }
